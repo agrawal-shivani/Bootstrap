ce0eb68f91cc2515ee495a19ab818321324d320f
shivani agrawal <55974920+agrawal-shivani@users.noreply.github.com>
2020-03-17 11:50:52 +0530

    Add files via upload

---
+++ lists.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Brown Fox</title>
+    </head>
+    <body>
+
+<p> Shopping List</p>
+<!--
+Add ordered list below
+-->
+		<ol>
+			<li>Milk</li>
+			<li>Detergent powder</li>
+			<li>bread</li>
+			<li>rice</li>
+			<li>dal</li>
+			<li>paneer</li>
+			<li>eggs</li>
+			<li>cheese</li>
+			<li>butter</li>
+		</ol>
+
+<!--
+Add unordered list below
+-->
+		<ul>
+			<li>Milk</li>
+			<li>Detergent powder</li>
+			<li>bread</li>
+			<li>rice</li>
+			<li>dal</li>
+			<li>paneer</li>
+			<li>eggs</li>
+			<li>cheese</li>
+			<li>butter</li>
+		</ul>
+     
+    </body>
+</html>
