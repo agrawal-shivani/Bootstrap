9b9fa9dbdbcb84d3e7c04a971f4bbd9161f8a8ff
shivani agrawal <55974920+agrawal-shivani@users.noreply.github.com>
2020-03-25 12:12:37 +0530

    Add files via upload

---
+++ colors.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Favourite Superhero</title>
+    <style>
+      p{
+        background-color:#800080;
+        color:#C0C0C0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>BATMAN</h1>
+    <p>A silent guardian, a watchful protector. A dark knight.</p>
+  </body>
+</html>
