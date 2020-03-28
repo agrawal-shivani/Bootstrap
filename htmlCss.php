15f8e4142237866d4f7731b73b221a3c1857667e
shivani agrawal <55974920+agrawal-shivani@users.noreply.github.com>
2020-03-28 13:57:49 +0530

    Add files via upload

---
+++ htmlCss.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>My Favorite Superhero</title>
+  </head>
+  <body>
+  <h1> Batman </h1>
+  <p><strong>Batman</strong> is a fictional comic character created by artist <u>Bob Kane</u> and writer<u>Bill Finger</u>.<br>
+
+Batman first appeared in DC comics in the year 1937 <del>and soon became popular</del>.</p>
+  </body>
+</html>
