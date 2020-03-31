9be9086c9b632bbd54c2937012f1e73dce977387
shivani agrawal <55974920+agrawal-shivani@users.noreply.github.com>
2020-03-31 10:21:57 +0530

    Add files via upload

---
+++ Fonts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Favourite Superhero</title>
+      <style>
+        h4{
+          font: normal bold 40px Open Sans;
+        }
+        p{
+          font: italic normal 23px Courier New;
+        }	
+    </style>
+  </head>
+  <body>
+    <h4>BATMAN</h4>
+    <p>Batman is a fictional superhero appearing in American comic books published by DC Comics.<br> The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27 (May 1939). Originally named the "Bat-Man", the character is also referred to by such epithets as the Caped Crusader, the Dark Knight, and the World's Greatest Detective.</p>
+  </body>
+</html>
